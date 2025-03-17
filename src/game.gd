dab26d278696ec094d84ee6087e6c250e082ca01
fragskye <199886693+fragskye@users.noreply.github.com>
2025-03-17 02:00:46 -0700

    Add convenience store

--- src/game.gd
+++ src/game.gd
@@ -4,20 +4,28 @@ const DEMON: PackedScene = preload("res://prefabs/demon.tscn")
 const BATTLE_LAYER: PackedScene = preload("res://prefabs/ui/battle_layer.tscn")
 
 @onready var battle_layer: BattleLayer = %BattleLayer
+@onready var spawn_demon_area: Area3D = %SpawnDemonArea
 
 @export var demons: Array[DemonData] = []
 @export var demon_index: int = 0
-@export var time_between_demons: float = 0.0
+@export var time_between_demons: float = 0.0 ## Starts counting after the player leaves the street
 
 var demon: Demon = null
+
+var _can_spawn_demon: bool = true
+var _in_spawn_demon_area: bool = false
 
 func _ready() -> void:
 	SignalBus.battle_won.connect(_on_battle_won)
 	SignalBus.battle_lost.connect(_on_battle_lost)
-	await get_tree().create_timer(2.0).timeout
-	spawn_next_demon()
+
+func _process(delta: float) -> void:
+	if _can_spawn_demon && _in_spawn_demon_area:
+		spawn_next_demon()
 
 func spawn_next_demon() -> void:
+	_can_spawn_demon = false
+	
 	# SHHHHHHHHHHHHHHHHHH i don't want to think how to reset the wheel properly right now, it has a lot of state in it
 	battle_layer.queue_free()
 	battle_layer = BATTLE_LAYER.instantiate()
@@ -37,7 +45,15 @@ func _on_battle_won() -> void:
 	demon_index += 1
 	if demon_index < demons.size():
 		NotificationLayer.show_toast("More demons will be back soon. I need to prepare.")
-	get_tree().create_timer(time_between_demons).timeout.connect(spawn_next_demon)
+	await spawn_demon_area.body_exited
+	await get_tree().create_timer(time_between_demons).timeout
+	_can_spawn_demon = true
 
 func _on_battle_lost() -> void:
 	pass
+
+func _on_spawn_demon_area_body_entered(body: Node3D) -> void:
+	_in_spawn_demon_area = true
+
+func _on_spawn_demon_area_body_exited(body: Node3D) -> void:
+	_in_spawn_demon_area = false
