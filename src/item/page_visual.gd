382d896491bd955ea6ec08d293c39df58971cf6c
fragskye <199886693+fragskye@users.noreply.github.com>
2025-03-20 06:17:16 -0700

    Add page burning VFX

--- src/item/page_visual.gd
+++ src/item/page_visual.gd
@@ -5,6 +5,9 @@ class_name PageVisual extends ItemVisual
 @onready var multiplier_label_shadow: Label3D = %MultiplierLabelShadow
 @onready var burning_label: Label3D = %BurningLabel
 @onready var pending_burn_label: Label3D = %PendingBurnLabel
+@onready var burning_0_particles: GPUParticles3D = %Burning0Particles
+@onready var burning_1_particles: GPUParticles3D = %Burning1Particles
+@onready var burning_2_particles: GPUParticles3D = %Burning2Particles
 
 var _page_data: PageData = null
 
@@ -21,11 +24,23 @@ func update_visual() -> void:
 	page_mesh.set_instance_shader_parameter("page", roundi(_page_data.multiplier))
 	multiplier_label.text = "%.fx" % _page_data.multiplier
 	multiplier_label_shadow.text = multiplier_label.text
-	if _page_data.pending_burn:
-		pending_burn_label.show()
-	else:
-		pending_burn_label.hide()
-	if _page_data.burning:
-		burning_label.show()
-	else:
-		burning_label.hide()
+	#if _page_data.pending_burn:
+	#	pending_burn_label.show()
+	#else:
+	#	pending_burn_label.hide()
+	#if _page_data.burning:
+	#	burning_label.show()
+	#else:
+	#	burning_label.hide()
+	if _page_data.pending_burn && !_page_data.burning:
+		burning_0_particles.show()
+		burning_1_particles.hide()
+		burning_2_particles.hide()
+	elif _page_data.burning && !_page_data.pending_burn:
+		burning_0_particles.hide()
+		burning_1_particles.show()
+		burning_2_particles.hide()
+	elif _page_data.pending_burn && _page_data.burning:
+		burning_0_particles.hide()
+		burning_1_particles.hide()
+		burning_2_particles.show()
