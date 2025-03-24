93c50c25c7c3764aecf32617835d92978f943bd4
fragskye <199886693+fragskye@users.noreply.github.com>
2025-03-24 07:18:52 -0700

    Overhaul lighting

--- src/item/page_visual.gd
+++ src/item/page_visual.gd
@@ -1,10 +1,6 @@
 class_name PageVisual extends ItemVisual
 
 @onready var page_mesh: MeshInstance3D = %PageMesh
-@onready var multiplier_label: Label3D = %MultiplierLabel
-@onready var multiplier_label_shadow: Label3D = %MultiplierLabelShadow
-@onready var burning_label: Label3D = %BurningLabel
-@onready var pending_burn_label: Label3D = %PendingBurnLabel
 @onready var burning_0_particles: GPUParticles3D = %Burning0Particles
 @onready var burning_1_particles: GPUParticles3D = %Burning1Particles
 @onready var burning_2_particles: GPUParticles3D = %Burning2Particles
@@ -25,16 +21,6 @@ func update_visual() -> void:
 	if _page_data == null:
 		return
 	page_mesh.set_instance_shader_parameter("page", roundi(_page_data.multiplier))
-	multiplier_label.text = "%.fx" % _page_data.multiplier
-	multiplier_label_shadow.text = multiplier_label.text
-	#if _page_data.pending_burn:
-	#	pending_burn_label.show()
-	#else:
-	#	pending_burn_label.hide()
-	#if _page_data.burning:
-	#	burning_label.show()
-	#else:
-	#	burning_label.hide()
 	if _page_data.pending_burn && !_page_data.burning:
 		burning_0_particles.show()
 		burning_1_particles.hide()
