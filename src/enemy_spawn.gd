4ca302f81fbfa5e6ce264098b580cb800182a040
fragskye <199886693+fragskye@users.noreply.github.com>
2025-03-29 07:03:31 -0700

    Support continuous enemy spawning

--- src/enemy_spawn.gd
+++ src/enemy_spawn.gd
@@ -6,14 +6,23 @@ const ENEMY: PackedScene = preload("res://prefabs/enemy.tscn")
 
 var delay: float = 0.0
 
+var player_entered: bool = false
+
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
+	body_exited.connect(_on_body_exited)
 
 func _process(delta: float) -> void:
 	if delay > 0.0:
 		delay = maxf(0.0, delay - delta)
+	
+	if player_entered:
+		if delay <= 0.0:
+			delay = cooldown
+			add_child(ENEMY.instantiate())
 
 func _on_body_entered(_body: Node3D) -> void:
-	if delay <= 0.0:
-		delay = cooldown
-		add_child(ENEMY.instantiate())
+	player_entered = true
+
+func _on_body_exited(_body: Node3D) -> void:
+	player_entered = false
